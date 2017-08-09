3b47f968c40d1eb673deaf4f864401f3e7fd4708
Rahul Sethuram <rksethuram9@gmail.com>
2017-08-08 18:30:13 -0700

    Make route name correct

--- scripts/chargeCard.js
+++ scripts/chargeCard.js
@@ -1,35 +1,34 @@
-var request = require('request');
+var request = require("request");
 
 var chargeCard = function(key, secret, token, amount, callback) {
+  if (!token || token.cvv == null) {
+    console.log("Error: check card information");
+    return callback({ error: "check card information" }, null);
+  }
 
-	if(!token || token.cvv == null) {
-		console.log("Error: check card information");
-		return callback({error: "check card information"}, null);
-	}
+  if (!amount) {
+    console.log("Error: no amount entered");
+    return callback({ error: "no amount entered" }, null);
+  }
 
-	if(!amount) {
-		console.log("Error: no amount entered");
-		return callback({error: "no amount entered"}, null);
-	}
+  var options = {
+    url: "http://api.connextapi.com/charge/",
+    method: "POST",
+    json: true,
+    body: {
+      token: token,
+      amount: amount,
+      key: key,
+      secret: secret
+    }
+  };
 
-	var options = {
-	  url: 'http://api.connextapi.com:3000/chargeCard/',
-	  method: 'POST',
-	  json: true,
-	  body: {
-	  	"token": token,
-	  	"amount": amount,
-	  	"key": key,
-	  	"secret": secret
-	  }
-	};
-
-	request(options, function(error, response, body) {
-		if(!error && response.statusCode == 200) {
-			console.log(body)
-			callback(error, body);
-		} else callback(error, null);
-	})
-}
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      callback(error, body);
+    } else callback(error, null);
+  });
+};
 
 module.exports = chargeCard;
