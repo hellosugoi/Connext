5947a58294d154120153928198f4060cfc714032
Rahul Sethuram <rksethuram9@gmail.com>
2017-08-08 18:30:28 -0700

    Fix API path

--- scripts/tokenize.js
+++ scripts/tokenize.js
@@ -1,28 +1,27 @@
-var request = require('request');
+var request = require("request");
 
 var tokenize = function(key, secret, token, callback) {
+  if (!token) {
+    console.log("Error: no token provided");
+    return callback({ error: "no token provided" }, null);
+  }
 
-	if(!token){
-		console.log("Error: no token provided");
-		return callback({error: "no token provided"}, null)
-	}
+  var options = {
+    url: "http://api.connextapi.com/tokenize/",
+    method: "POST",
+    json: true,
+    body: {
+      token: token,
+      key: key,
+      secret: secret
+    }
+  };
 
-	var options = {
-	  url: 'http://api.connextapi.com:3000/tokenize/',
-	  method: 'POST',
-	  json: true,
-	  body: {
-	  	"token": token,
-	  	"key": key,
-	  	"secret": secret
-	  }
-	};
-
-	request(options, function(error, response, body) {
-		if(!error && response.statusCode == 200) {
-			callback(error, body);
-		} else console.log(error, null);
-	})
-}
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(error, body);
+    } else console.log(error, null);
+  });
+};
 
 module.exports = tokenize;
